4ff4eb07135ec02c3710e6602af2889d65efc14f
Sam Kern <samkern108@gmail.com>
2017-04-04 20:03:40 -0700

    New level prefabs and moved some scripts.

---
+++ Assets/Scripts/UI/PressToRetry.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressToRetry : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			LevelMaster.Retry ();
+			gameObject.SetActive (false);
+		}	
+	}
+}
